62e0945ba59932fbbcba2b27f0b083beba50584e
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2023-03-28 10:40:49 +0530

    Mar 28th Notes and Programs

--- programs/Hello.java
+++ programs/Hello.java
@@ -1,9 +1,16 @@
+//This is a Java Program to print "Hello World" on console
+/* 
+This is developed by Riyaz 
+Reviewed by RangaSwamy
+Reviewed on 28-Mar-2023
+*/
 public class Hello
 {
 
 public static void main(String[] args)
 {
 
+//This statement is to print something on console
 System.out.println("Hello World ..Welcome to Java ...");
 
 
---
+++ programs/Sum.java
@@ -0,0 +1,32 @@
+//This is a Java pgm to add two int values
+//import java.util.*;
+
+import java.util.Scanner;
+
+public class Sum 
+{
+
+public static void main(String[] args)
+{
+
+
+
+System.out.println("Enter value of x :");
+
+Scanner sc=new Scanner(System.in);
+
+int x=sc.nextInt();
+
+System.out.println("Enter value of y :");
+
+Scanner sc1=new Scanner(System.in);
+
+int y=sc1.nextInt();
+
+
+System.out.println("Sum is :"+(x+y));
+
+}
+
+
+}
