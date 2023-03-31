77f88936ed9b06f1a71b95aee11e00a3c3e8c812
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2023-03-31 10:08:54 +0530

    Mar 31st Notes

---
+++ programs/VariablesDemo1.java
@@ -0,0 +1,26 @@
+public class VariablesDemo1
+{
+
+public static void main(String[] args)
+{
+
+int x=10;
+
+int y=0;
+
+
+System.out.println("x is "+x);
+
+System.out.println("y is "+y);
+
+System.out.println(args[0]);
+
+
+System.out.println(args[1]);
+
+
+}
+
+
+
+}
